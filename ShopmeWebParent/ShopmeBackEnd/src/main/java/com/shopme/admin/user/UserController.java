d23797434115506ae8fed373d37cf97d08fd4d3d
trombone8417 <trombone8417@yahoo.com>
2021-11-13 05:05:37 +0800

    Code New User Function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Controller
@@ -25,15 +27,21 @@ public class UserController {
 	
 	@GetMapping("/users/new")
 	public String newUser(Model model) {
+		List<Role> listRoles = service.listRoles();
+		
 		User user = new User();
+		user.setEnabled(true);
 		model.addAttribute("user",user);
+		model.addAttribute("listRoles",listRoles);
 		
 		return "user_form";
 	}
 	
 	@PostMapping("/users/save")
-	public String saveUser(User user) {
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
 		System.out.println(user);
+		service.save(user);
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
 		return "redirect:/users";
 	}
 }
