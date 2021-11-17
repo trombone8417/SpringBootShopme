c448b47ff99f8d9690250af3a45fbe4f7d86212a
trombone8417 <trombone8417@yahoo.com>
2021-11-17 18:43:54 +0800

    Code Filter Function for Users v1

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -29,16 +29,16 @@ public class UserController {
 	
 	@GetMapping("/users")
 	public String listAll(Model model) {
-		return listByPage(1, model, "firstName","asc");
+		return listByPage(1, model, "firstName","asc",null);
 	}
 	
 	@GetMapping("/users/page/{pageNum}")
-	public String listByPage(@PathVariable(name="pageNum") int pageNum, Model model,@Param("sortField" ) String sortField, @Param("sortDir") String sortDir) {
+	public String listByPage(@PathVariable(name="pageNum") int pageNum, Model model,@Param("sortField" ) String sortField, @Param("sortDir") String sortDir,@Param("keyword") String keyword) {
 		
 		System.out.println("Sort Field: " + sortField);
 		System.out.println("Sort Order: " + sortDir);
 		
-		Page<User> page = service.listByPage(pageNum, sortField,sortDir);
+		Page<User> page = service.listByPage(pageNum, sortField,sortDir,keyword);
 		List<User> listUsers = page.getContent();
 		
 		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
@@ -58,6 +58,7 @@ public class UserController {
 		model.addAttribute("sortField",sortField);
 		model.addAttribute("sortDir",sortDir);
 		model.addAttribute("reverseSortDir",reverseSortDir);
+		model.addAttribute("keyword",keyword);
 		return "users";
 	}
 	
