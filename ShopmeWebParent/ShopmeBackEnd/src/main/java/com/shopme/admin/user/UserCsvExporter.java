e9b14bd53506daf2ac6beabca017e0a1ecc4e6bf
trombone8417 <trombone8417@yahoo.com>
2021-11-18 20:40:56 +0800

    Code Export Users to Excel

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserCsvExporter.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserCsvExporter.java
@@ -15,17 +15,9 @@ import org.supercsv.prefs.CsvPreference;
 
 import com.shopme.common.entity.User;
 
-public class UserCsvExporter {
+public class UserCsvExporter extends AbstractExporter {
 	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
-		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-		String timestamp = dateFormatter.format(new Date());
-		String fileName = "users_" + timestamp + ".csv";
-		
-		response.setContentType("text/csv");
-		
-		String headerKey = "Content-Disposition";
-		String headerValue = "attachment; filename=" + fileName;
-		response.setHeader(headerKey, headerValue);
+		super.setResponseHeader(response, "text/csv", ".csv");
 		
 		ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
 		
