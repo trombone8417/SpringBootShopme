dafed9d1ac2b6982b843b8cb02c334f5be013b67
trombone8417 <trombone8417@yahoo.com>
2021-11-21 16:13:47 +0800

    Understand Technical Design for Category Module

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
@@ -7,9 +7,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileUploadUtil {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
 	
 	public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException{
 		Path uploadPath = Paths.get(uploadDir);
@@ -35,12 +39,14 @@ public class FileUploadUtil {
 					try {
 						Files.delete(file);
 					}catch (IOException ex) {
+						LOGGER.error("Could not delete file: " + file);
 						System.out.println("Could not delete file: " + file);
 					}
 				}
 			});
 			
 		} catch (IOException ex) {
+			LOGGER.error("Could not list directory: " + dirPath);
 			System.out.println("Could not list directory: " + dirPath);
 		}
 	}
