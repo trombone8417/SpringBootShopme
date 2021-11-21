a2be696199e2f826eb677ae8e271ba8e4fea8ea8
trombone8417 <trombone8417@yahoo.com>
2021-11-21 08:33:25 +0800

    Code Refactor #3

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/controller/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.shopme.admin.user;
+package com.shopme.admin.user.controller;
 
 import java.io.IOException;
 import java.util.List;
@@ -20,6 +20,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.FileUploadUtil;
+import com.shopme.admin.user.UserNotFoundException;
+import com.shopme.admin.user.UserService;
 import com.shopme.admin.user.export.UserCsvExporter;
 import com.shopme.admin.user.export.UserExcelExporter;
 import com.shopme.admin.user.export.UserPdfExporter;
@@ -64,7 +66,7 @@ public class UserController {
 		model.addAttribute("sortDir",sortDir);
 		model.addAttribute("reverseSortDir",reverseSortDir);
 		model.addAttribute("keyword",keyword);
-		return "users";
+		return "users/users";
 	}
 	
 	@GetMapping("/users/new")
@@ -77,7 +79,7 @@ public class UserController {
 		model.addAttribute("listRoles",listRoles);
 		model.addAttribute("pageTitle", "Create New User");
 		
-		return "user_form";
+		return "/users/user_form";
 	}
 	
 	@PostMapping("/users/save")
@@ -117,7 +119,7 @@ public class UserController {
 			model.addAttribute("user",user);
 			model.addAttribute("pageTitle", "Edit User (ID: "+ id + ")");
 			model.addAttribute("listRoles",listRoles);
-			return "user_form";
+			return "/users/user_form";
 		} catch (UserNotFoundException ex) {
 			redirectAttributes.addFlashAttribute("message", ex.getMessage());
 			return "redirect:/users";
