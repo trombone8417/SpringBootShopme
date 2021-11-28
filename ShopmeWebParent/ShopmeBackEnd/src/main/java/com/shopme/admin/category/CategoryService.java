166ba65f23c8bfb5a3e0efc78d82df822180ca7c
trombone8417 <trombone8417@yahoo.com>
2021-11-28 19:39:17 +0800

    Exercise: Code Update Category Function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -2,6 +2,7 @@ package com.shopme.admin.category;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -93,6 +94,14 @@ public class CategoryService {
 			categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
 
 			listSubCategoriesUsedInForm(categoriesUsedInForm, subCategory, newSubLevel);
+		}
+	}
+	
+	public Category get(Integer id) throws CategoryNotFoundException {
+		try {
+			return repo.findById(id).get();
+		} catch (NoSuchElementException ex) {
+			throw new CategoryNotFoundException("Could not find any category with ID " + id);
 		}
 	}
 }
