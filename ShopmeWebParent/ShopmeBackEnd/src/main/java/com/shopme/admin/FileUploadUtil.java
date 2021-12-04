2af28f59b2eae22a2a50beb25da0218fd4a25515
trombone8417 <trombone8417@yahoo.com>
2021-12-04 09:07:37 +0800

    Exercise: Code Delete Category Function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
@@ -50,5 +50,16 @@ public class FileUploadUtil {
 			System.out.println("Could not list directory: " + dirPath);
 		}
 	}
+	
+	public static void removeDir(String dir) {
+		cleanDir(dir);
+		
+		try {
+			Files.delete(Paths.get(dir));
+		} catch (IOException e) {
+			LOGGER.error("Could not remove directory: " + dir);
+		}
+		
+	}
 
 }
