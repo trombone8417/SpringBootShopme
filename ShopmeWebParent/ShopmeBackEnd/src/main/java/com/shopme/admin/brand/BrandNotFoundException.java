6e6d97e69c34fa6f62ae87a22aa2219abab30096
trombone8417 <trombone8417@yahoo.com>
2021-12-13 18:34:24 +0800

    Exercise: Code Create, Update and Delete Brand Function

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandNotFoundException.java
@@ -0,0 +1,6 @@
+package com.shopme.admin.brand;
+public class BrandNotFoundException extends Exception {
+	public BrandNotFoundException(String message) {
+		super(message);
+	}
+}
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import com.shopme.common.entity.Brand;
 
 public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
-
+	public Long countById(Integer id);
 }
