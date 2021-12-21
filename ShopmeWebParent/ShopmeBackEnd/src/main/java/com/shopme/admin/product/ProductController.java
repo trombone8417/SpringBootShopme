a1a5056ce314bde8915a8f57f015ef916f97bcac
trombone8417 <trombone8417@yahoo.com>
2021-12-21 19:17:49 +0800

    Code Product Overview Form v1

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -7,12 +7,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.shopme.admin.brand.BrandService;
+import com.shopme.common.entity.Brand;
 import com.shopme.common.entity.Product;
 
 @Controller
 public class ProductController {
 	@Autowired
 	private ProductService productService;
+	
+	@Autowired
+	private BrandService brandService;
 	
 	@GetMapping("/products")
 	public String listAll(Model model) {
@@ -21,6 +26,20 @@ public class ProductController {
 		model.addAttribute("listProducts", listProducts);
 		
 		return "products/products";
+	}
+	
+	@GetMapping("/products/new")
+	public String newProduct(Model model) {
+		List<Brand> listBrands = brandService.listAll();
+		Product product = new Product();
+		product.setEnabled(true);
+		product.setInStock(true);
+		
+		model.addAttribute("product",product);
+		model.addAttribute("listBrands",listBrands);
+		model.addAttribute("pageTitle", "Create New Product");
+		
+		return "products/product_form";
 	}
 
 }
