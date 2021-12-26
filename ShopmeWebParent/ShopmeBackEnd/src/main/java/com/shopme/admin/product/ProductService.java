ca22c9d6c4d6f0e54e0e6f69a412e5a8c385104b
trombone8417 <trombone8417@yahoo.com>
2021-12-26 17:10:10 +0800

    Code Save Product Function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
@@ -1,9 +1,12 @@
 package com.shopme.admin.product;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.common.entity.Product;
 
@@ -16,4 +19,26 @@ public class ProductService {
 	public List<Product> listAll(){
 		return (List<Product>) repo.findAll();
 	}
+	
+	public Product save(Product product) {
+		if (product.getId() == null) {
+			product.setCreatedTime(new Date());
+		}
+		
+		if (product.getAlias() == null || product.getAlias().isEmpty()) {
+			String defaultAlias = product.getName().replaceAll(" ", "-");
+			product.setAlias(defaultAlias);
+		} else {
+			product.setAlias(product.getAlias().replaceAll(" ", "-"));
+		}
+		
+		product.setUpdatedTime(new Date());
+		
+		return repo.save(product);
+	}
 }
+
+
+
+
+
