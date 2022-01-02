4fcde7e98a0cfc32f9312504ad246ce324ed014a
trombone8417 <trombone8417@yahoo.com>
2022-01-02 10:11:42 +0800

    Exercise: Code Product Detail Entity Class

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -1,7 +1,9 @@
 package com.shopme.common.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -73,6 +75,10 @@ public class Product {
 	
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
 	private Set<ProductImage> images = new HashSet<>();
+	
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+	private List<ProductDetail> details = new ArrayList<>();
+
 	
 	public Integer getId() {
 		return id;
@@ -213,6 +219,18 @@ public class Product {
 		if(id == null || mainImage == null) return "/images/image-thumbnail.png";
 		
 		return "/product-images/" + this.id + "/" + this.mainImage;
+	}
+	
+
+	public List<ProductDetail> getDetails() {
+		return details;
+	}
+	public void setDetails(List<ProductDetail> details) {
+		this.details = details;
+	}
+	
+	public void addDetail(String name, String value) {
+		this.details.add(new ProductDetail(name, value, this));
 	}
 }
 
