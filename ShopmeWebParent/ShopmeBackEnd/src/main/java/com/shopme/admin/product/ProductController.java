8731e30ab29951b62d234621f956ea4899f2fd04
trombone8417 <trombone8417@yahoo.com>
2022-01-03 18:33:46 +0800

    Code Save Product's Details (Specs)

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -53,13 +53,16 @@ public class ProductController {
 	@PostMapping("/products/save")
 	public String saveProduct(Product product, RedirectAttributes ra, 
 			@RequestParam("fileImage") MultipartFile mainImageMultipart,
-			@RequestParam("extraImage") MultipartFile[] extraImageMultiparts
+			@RequestParam("extraImage") MultipartFile[] extraImageMultiparts,
+			@RequestParam(name = "detailNames", required = false) String[] detailNames,
+			@RequestParam(name = "detailValues", required = false) String[] detailValues
 			) throws IOException {
 		setMainImageName(mainImageMultipart, product);
 		setExtraImageNames(extraImageMultiparts, product);
+		setProductDetails(detailNames, detailValues, product);
 			
-			Product savedProduct = productService.save(product);
-			saveUploadedImages(mainImageMultipart, extraImageMultiparts, savedProduct);
+		Product savedProduct = productService.save(product);
+		saveUploadedImages(mainImageMultipart, extraImageMultiparts, savedProduct);
 
 
 		ra.addFlashAttribute("message", "The product has been saved successfully.");
@@ -67,6 +70,20 @@ public class ProductController {
 		return "redirect:/products";
 	}
 	
+	private void setProductDetails(String[] detailNames, String[] detailValues, Product product) {
+		if (detailNames == null || detailNames.length == 0) return;
+		
+		for (int count = 0; count<detailNames.length; count++) {
+			String name = detailNames[count];
+			String value = detailValues[count];
+			
+			if (!name.isEmpty() && !value.isEmpty()) {
+				product.addDetail(name, value);
+			}
+		}
+		
+	}
+
 	private void saveUploadedImages(MultipartFile mainImageMultipart, 
 			MultipartFile[] extraImageMultiparts,
 			Product savedProduct) throws IOException {
