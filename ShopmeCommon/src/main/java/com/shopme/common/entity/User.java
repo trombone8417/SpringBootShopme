e2840a7a7d820baef3f6b684301e7ce2e07b6bcb
trombone8417 <trombone8417@yahoo.com>
2022-01-15 06:30:02 +0800

    Code Authorization for Product Module v3

--- ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -1,6 +1,7 @@
 package com.shopme.common.entity;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import javax.persistence.Column;
@@ -145,4 +146,26 @@ public class User {
 	public String getFullName() {
 		return firstName + " " + lastName;
 	}
+	
+	public boolean hasRole(String roleName) {
+		Iterator<Role> iterator = roles.iterator();
+		
+		while (iterator.hasNext()) {
+			Role role = iterator.next();
+			if (role.getName().equals(roleName)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }
+
+
+
+
+
+
+
+
+
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
@@ -13,7 +13,8 @@ import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 public class ShopmeUserDetails implements UserDetails {
-	
+
+	private static final long serialVersionUID = 1L;
 	private User user;
 
 	public ShopmeUserDetails(User user) {
@@ -73,6 +74,10 @@ public class ShopmeUserDetails implements UserDetails {
 	
 	public void setLastName(String lastName) {
 		this.user.setLastName(lastName);
+	}
+	
+	public boolean hasRole(String roleName) {
+		return user.hasRole(roleName);
 	}
 
 }
