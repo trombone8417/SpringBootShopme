f5115a05cc8dc510df745c67201175e23b063d7d
trombone8417 <trombone8417@yahoo.com>
2022-05-29 09:34:51 +0800

    Code Manage Countries v4

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/countries_setting.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/countries_setting.js
@@ -23,8 +23,45 @@ $(document).ready(function() {
 
     dropDownCountry.on("change", function() {
         changeFromStateToSelectedCountry();
-    })
+    });
+
+    buttonAddCountry.click(function() {
+        if (buttonAddCountry.val() == "Add") {
+            addCountry();
+        } else {
+            changeFromStateToNew();
+        }
+    });
 });
+
+function addCountry() {
+    url = contextPath + "countries/save";
+    countryName = fieldCountryName.val();
+    countryCode = fieldCountryCode.val();
+    jsonData = { name: countryName, code: countryCode };
+    $.ajax({
+        type: "POST",
+        url: url,
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader(csrfHeaderName, csrfValue);
+        },
+        data: JSON.stringify(jsonData),
+        contentType: "application/json"
+    }).done(function(countryId) {
+        alert("newly added country ID: " + countryId);
+    })
+}
+
+function changeFromStateToNew() {
+    buttonAddCountry.val("Add");
+    labelCountryName.text("Country Name");
+
+    buttonUpdateCountry.prop("disabled", false);
+    buttonDeleteCountry.prop("disabled", false);
+
+    fieldCountryCode.val("");
+    fieldCountryName.val("").focus();
+}
 
 function changeFromStateToSelectedCountry() {
     buttonAddCountry.prop("value", "New");
