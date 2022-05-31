4917a7ff4ad9bb2b2831096e852a3225daa6a947
trombone8417 <trombone8417@yahoo.com>
2022-05-31 19:25:01 +0800

    Exercise: Code Controller Layer for State/Province

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Country.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Country.java
@@ -43,6 +43,11 @@ public class Country {
 	public Country(String name) {
 		this.name = name;
 	}
+	
+	public Country(Integer id) {
+		this.id = id;
+	}
+
 	public Integer getId() {
 		return id;
 	}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/state/StateDTO.java
@@ -0,0 +1,33 @@
+package com.shopme.admin.setting.state;
+
+public class StateDTO {
+	private Integer id;
+	private String name;
+	
+	public StateDTO() {
+	}
+
+	public StateDTO(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
