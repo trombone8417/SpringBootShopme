34b9db927dd56c3074d6813d412e14b7f719b0ed
trombone8417 <trombone8417@yahoo.com>
2022-06-06 19:31:07 +0800

    Code Customer Registration Form v3

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Country;
+import com.shopme.common.entity.Customer;
 import com.shopme.setting.CountryRepository;
 
 @Service
@@ -16,6 +17,11 @@ public class CustomerService {
 	
 	public List<Country> listAllCountries(){
 		return countryRepo.findAllByOrderByNameAsc();
+	}
+	
+	public boolean isEmailUnique(String email) {
+		Customer customer = customerRepo.findByEmail(email);
+		return customer == null;
 	}
 }
 
--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/StateRestController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/StateRestController.java
@@ -1,4 +1,4 @@
-package com.shopme.admin.setting.state;
+package com.shopme.setting;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +20,7 @@ public class StateRestController {
 
 	@Autowired private StateRepository repo;
 	
-	@GetMapping("/states/list_by_country/{id}")
+	@GetMapping("/settings/list_states_by_country/{id}")
 	public List<StateDTO> listByCountry(@PathVariable("id") Integer countryId) {
 		List<State> listStates = repo.findByCountryOrderByNameAsc(new Country(countryId));
 		List<StateDTO> result = new ArrayList<>();
@@ -32,15 +32,4 @@ public class StateRestController {
 		return result;
 	}
 
-	
-	@PostMapping("/states/save")
-	public String save(@RequestBody State state) {
-		State savedState = repo.save(state);
-		return String.valueOf(savedState.getId());
-	}
-	
-	@DeleteMapping("/states/delete/{id}")
-	public void delete(@PathVariable("id") Integer id) {
-		repo.deleteById(id);
-	}
 }
