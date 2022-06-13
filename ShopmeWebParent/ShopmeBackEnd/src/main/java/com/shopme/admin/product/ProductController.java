38f20c19835a65903aff23c3d05d377fc1f45b48
trombone8417 <trombone8417@yahoo.com>
2022-06-13 19:25:58 +0800

    Exercise: Code Customer Management Module

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -1,22 +1,14 @@
 package com.shopme.admin.product;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,26 +23,18 @@ import com.shopme.admin.security.ShopmeUserDetails;
 import com.shopme.common.entity.Brand;
 import com.shopme.common.entity.Category;
 import com.shopme.common.entity.Product;
-import com.shopme.common.entity.ProductImage;
 import com.shopme.common.exception.ProductNotFoundException;
 
 @Controller
 public class ProductController {
-	
-	@Autowired
-	private ProductService productService;
-	
-	@Autowired
-	private BrandService brandService;
-	
-	@Autowired
-	private CategoryService categoryService;
+	@Autowired private ProductService productService;
+	@Autowired private BrandService brandService;
+	@Autowired private CategoryService categoryService;
 	
 	@GetMapping("/products")
 	public String listFirstPage(Model model) {
 		return listByPage(1, model, "name", "asc", null, 0);
 	}
-	
 	
 	@GetMapping("/products/page/{pageNum}")
 	public String listByPage(
@@ -72,8 +56,8 @@ public class ProductController {
 		
 		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
 		
-		if (categoryId != null) model.addAttribute("categoryId", categoryId);
-		
+		if (categoryId != null) model.addAttribute("categoryId", categoryId); 
+			
 		model.addAttribute("currentPage", pageNum);
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("startCount", startCount);
@@ -83,16 +67,16 @@ public class ProductController {
 		model.addAttribute("sortDir", sortDir);
 		model.addAttribute("reverseSortDir", reverseSortDir);
 		model.addAttribute("keyword", keyword);		
-		model.addAttribute("listProducts", listProducts);	
-		model.addAttribute("listCategories", listCategories);
+		model.addAttribute("listProducts", listProducts);
+		model.addAttribute("listCategories", listCategories);		
 		
 		return "products/products";		
 	}
 	
-	
 	@GetMapping("/products/new")
 	public String newProduct(Model model) {
 		List<Brand> listBrands = brandService.listAll();
+		
 		Product product = new Product();
 		product.setEnabled(true);
 		product.setInStock(true);
@@ -106,8 +90,8 @@ public class ProductController {
 	}
 	
 	@PostMapping("/products/save")
-	public String saveProduct(Product product, RedirectAttributes ra, 
-			@RequestParam(value = "fileImage", required = false) MultipartFile mainImageMultipart,
+	public String saveProduct(Product product, RedirectAttributes ra,
+			@RequestParam(value = "fileImage", required = false) MultipartFile mainImageMultipart,			
 			@RequestParam(value = "extraImage", required = false) MultipartFile[] extraImageMultiparts,
 			@RequestParam(name = "detailIDs", required = false) String[] detailIDs,
 			@RequestParam(name = "detailNames", required = false) String[] detailNames,
@@ -115,34 +99,33 @@ public class ProductController {
 			@RequestParam(name = "imageIDs", required = false) String[] imageIDs,
 			@RequestParam(name = "imageNames", required = false) String[] imageNames,
 			@AuthenticationPrincipal ShopmeUserDetails loggedUser
-			) throws IOException {
+			) 
+					throws IOException {
 		if (loggedUser.hasRole("Salesperson")) {
 			productService.saveProductPrice(product);
-
-			ra.addFlashAttribute("message", "The product has been saved successfully.");
-			
-			return "redirect:/products";
+			ra.addFlashAttribute("message", "The product has been saved successfully.");			
+			return "redirect:/products";			
 		}
 		
 		ProductSaveHelper.setMainImageName(mainImageMultipart, product);
-		ProductSaveHelper.setExistingExtraImageNames(imageIDs,imageNames,product);
+		ProductSaveHelper.setExistingExtraImageNames(imageIDs, imageNames, product);
 		ProductSaveHelper.setNewExtraImageNames(extraImageMultiparts, product);
-		ProductSaveHelper.setProductDetails(detailIDs,detailNames, detailValues, product);
+		ProductSaveHelper.setProductDetails(detailIDs, detailNames, detailValues, product);
 			
 		Product savedProduct = productService.save(product);
+		
 		ProductSaveHelper.saveUploadedImages(mainImageMultipart, extraImageMultiparts, savedProduct);
-
-		ProductSaveHelper.deleteExtraImagesWereRemovedOnForm(product);
-
+		
+		ProductSaveHelper.deleteExtraImagesWeredRemovedOnForm(product);
+		
 		ra.addFlashAttribute("message", "The product has been saved successfully.");
 		
 		return "redirect:/products";
 	}
-	
-	
+
 	
 	@GetMapping("/products/{id}/enabled/{status}")
-	public String updateCategoryEnabledStatus(@PathVariable("id") Integer id,
+	public String updateProductEnabledStatus(@PathVariable("id") Integer id,
 			@PathVariable("status") boolean enabled, RedirectAttributes redirectAttributes) {
 		productService.updateProductEnabledStatus(id, enabled);
 		String status = enabled ? "enabled" : "disabled";
@@ -152,15 +135,15 @@ public class ProductController {
 		return "redirect:/products";
 	}
 	
-	
 	@GetMapping("/products/delete/{id}")
 	public String deleteProduct(@PathVariable(name = "id") Integer id, 
 			Model model,
 			RedirectAttributes redirectAttributes) {
 		try {
 			productService.delete(id);
-			String productExtraImagesDir = "../product-images/" + id + "/extras";	
-			String productImagesDir = "../product-images/" + id ;				
+			String productExtraImagesDir = "../product-images/" + id + "/extras";
+			String productImagesDir = "../product-images/" + id;
+			
 			FileUploadUtil.removeDir(productExtraImagesDir);
 			FileUploadUtil.removeDir(productImagesDir);
 			
@@ -171,7 +154,7 @@ public class ProductController {
 		}
 		
 		return "redirect:/products";
-	}	
+	}
 	
 	@GetMapping("/products/edit/{id}")
 	public String editProduct(@PathVariable("id") Integer id, Model model,
@@ -186,29 +169,29 @@ public class ProductController {
 			model.addAttribute("pageTitle", "Edit Product (ID: " + id + ")");
 			model.addAttribute("numberOfExistingExtraImages", numberOfExistingExtraImages);
 			
+			
 			return "products/product_form";
+			
 		} catch (ProductNotFoundException e) {
 			ra.addFlashAttribute("message", e.getMessage());
-
+			
 			return "redirect:/products";
 		}
 	}
-	
 	
 	@GetMapping("/products/detail/{id}")
 	public String viewProductDetails(@PathVariable("id") Integer id, Model model,
 			RedirectAttributes ra) {
 		try {
-			Product product = productService.get(id);
-			
-			model.addAttribute("product", product);
+			Product product = productService.get(id);			
+			model.addAttribute("product", product);		
 			
 			return "products/product_detail_modal";
+			
 		} catch (ProductNotFoundException e) {
 			ra.addFlashAttribute("message", e.getMessage());
-
+			
 			return "redirect:/products";
 		}
-	}
-
+	}	
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -15,26 +15,25 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Bean
 	public UserDetailsService userDetailsService() {
 		return new ShopmeUserDetailsService();
 	}
 	
 	@Bean
-	public PasswordEncoder PasswordEncoder() {
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 		authProvider.setUserDetailsService(userDetailsService());
-		authProvider.setPasswordEncoder(PasswordEncoder());
+		authProvider.setPasswordEncoder(passwordEncoder());
 		
 		return authProvider;
 	}
 	
-	
-
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(authenticationProvider());
@@ -43,30 +42,40 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/users/**", "/settings/**", "/countries/**", "/states/**").hasAuthority("Admin")
-		.antMatchers("/categories/**", "/brands/**").hasAnyAuthority("Admin", "Editor")
-		.antMatchers("/products/new", "/products/delete/**").hasAnyAuthority("Admin", "Editor")
-		.antMatchers("/products/edit/**", "/products/save", "/products/check_unique")
-			.hasAnyAuthority("Admin", "Editor", "Salesperson")
-		.antMatchers("/products", "/products/", "/products/detail/**", "/products/page/**")
-			.hasAnyAuthority("Admin", "Editor", "Salesperson", "Shipper")
-		.antMatchers("/products/**").hasAnyAuthority("Admin", "Editor")
-		.anyRequest()
-		.authenticated()
-		.and()
-		.formLogin()
-			.loginPage("/login")
-			.usernameParameter("email")
-			.permitAll()
-		.and().logout().permitAll()
-		.and()
-			.rememberMe()
-				.key("AbcDefgHijKlmnOpqrs_1234567890")
-				.tokenValiditySeconds(7 * 24 * 60 * 60);
+			.antMatchers("/users/**", "/settings/**", "/countries/**", "/states/**").hasAuthority("Admin")
+			.antMatchers("/categories/**", "/brands/**").hasAnyAuthority("Admin", "Editor")
+			
+			.antMatchers("/products/new", "/products/delete/**").hasAnyAuthority("Admin", "Editor")
+			
+			.antMatchers("/products/edit/**", "/products/save", "/products/check_unique")
+				.hasAnyAuthority("Admin", "Editor", "Salesperson")
+				
+			.antMatchers("/products", "/products/", "/products/detail/**", "/products/page/**")
+				.hasAnyAuthority("Admin", "Editor", "Salesperson", "Shipper")
+				
+			.antMatchers("/products/**").hasAnyAuthority("Admin", "Editor")
+			
+			.antMatchers("/customers/**").hasAnyAuthority("Admin", "Salesperson")
+			
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()			
+				.loginPage("/login")
+				.usernameParameter("email")
+				.permitAll()
+			.and().logout().permitAll()
+			.and()
+				.rememberMe()
+					.key("AbcDefgHijKlmnOpqrs_1234567890")
+					.tokenValiditySeconds(7 * 24 * 60 * 60);
+					;
+			
 	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
 	}
+
+	
 }
