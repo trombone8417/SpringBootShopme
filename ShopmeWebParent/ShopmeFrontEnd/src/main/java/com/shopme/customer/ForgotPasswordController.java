2ec306164ccb5db260bb9b79eb7b47dbafeb464d
trombone8417 <trombone8417@yahoo.com>
2022-07-09 10:32:04 +0800

    Code Forgot Password Function v3

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/ForgotPasswordController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/ForgotPasswordController.java
@@ -1,18 +1,28 @@
 package com.shopme.customer;
 
+import java.io.UnsupportedEncodingException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.shopme.Utility;
 import com.shopme.common.exception.CustomerNotFoundException;
+import com.shopme.setting.EmailSettingBag;
+import com.shopme.setting.SettingService;
 
 @Controller
 public class ForgotPasswordController {
 	@Autowired private CustomerService customerService;
+	@Autowired private SettingService settingService;
 	
 	@GetMapping("/forgot_password")
 	public String showRequestForm() {
@@ -24,13 +34,44 @@ public class ForgotPasswordController {
 		String email = request.getParameter("email");
 		try {
 			String token = customerService.updateResetPasswordToken(email);
-			System.out.println("Email: " + email);
-			System.out.println("Token: " + token);
+			String link = Utility.getSiteURL(request) + "/reset_password?token=" + token;
+			model.addAttribute("message", "We have sent a reset password link to your email."
+					+ " Please check.");
+			sendEmail(link, email);
+			
 		} catch (CustomerNotFoundException e) {
 			model.addAttribute("error", e.getMessage());
-		}
+		} catch (UnsupportedEncodingException | MessagingException e) {
+			model.addAttribute("error", "Could not send email");
+		} 
 		
 		return "customer/forgot_password_form";
+	}
+	
+	private void sendEmail(String link, String email) throws UnsupportedEncodingException, MessagingException {
+		EmailSettingBag emailSettings = settingService.getEmailSettings();
+		JavaMailSenderImpl mailSender = Utility.prepareMailSender(emailSettings);
+		
+		String toAddress = email;
+		String subject = "Here's the link to reset your password";
+		
+		String content = "<p>Hello,</p>"
+				+ "<p>You have requested to reset your password.</p>"
+				+ "<p>Client the link below to change your password.</p>"
+				+ "<p><a href=\"" + link + "\">Change my password</a></p>"
+				+ "<br>"
+				+ "<p>Ignore this email if you do remember your password."
+				+ "or you have not made the request.</p>";
+		
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+		
+		helper.setFrom(emailSettings.getFromAddress(), emailSettings.getSenderName());
+		helper.setTo(toAddress);
+		helper.setSubject(subject);
+		
+		helper.setText(content, true);
+		mailSender.send(message);
 	}
 }
 
