af6cb3a3a8b82a76785227be16561c2c49a20e7a
trombone8417 <trombone8417@yahoo.com>
2022-08-19 21:44:50 +0800

    Code Refactor #12 v3

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Address.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Address.java
@@ -8,31 +8,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "addresses")
-public class Address extends IdBasedEntity {
-	
-	@Column(name = "first_name", nullable = false, length = 45)
-	private String firstName;
-	
-	@Column(name = "last_name", nullable = false, length = 45)
-	private String lastName;
-	
-	@Column(name = "phone_number", nullable = false, length = 15)
-	private String phoneNumber;
-	
-	@Column(name = "address_line_1", nullable = false, length = 64)
-	private String addressLine1;
-	
-	@Column(name = "address_line_2", length = 64)
-	private String addressLine2;
-	
-	@Column(nullable = false, length = 45)
-	private String city;
-	
-	@Column(nullable = false, length = 45)
-	private String state;
-	
-	@Column(name = "postal_code", nullable = false, length = 10)
-	private String postalCode;
+public class Address extends AbstractAddress {
 	
 	@ManyToOne
 	@JoinColumn(name = "country_id")
@@ -46,69 +22,6 @@ public class Address extends IdBasedEntity {
 	private boolean defaultForShipping;
 
 
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public String getAddressLine1() {
-		return addressLine1;
-	}
-
-	public void setAddressLine1(String addressLine1) {
-		this.addressLine1 = addressLine1;
-	}
-
-	public String getAddressLine2() {
-		return addressLine2;
-	}
-
-	public void setAddressLine2(String addressLine2) {
-		this.addressLine2 = addressLine2;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getPostalCode() {
-		return postalCode;
-	}
-
-	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
-	}
 
 	public Country getCountry() {
 		return country;
