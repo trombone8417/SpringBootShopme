89a3657960c74af0484f9a7764b05621131d1243
trombone8417 <trombone8417@yahoo.com>
2022-08-24 18:54:50 +0800

    Update Shopping Cart Page v3

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartController.java
@@ -10,16 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.shopme.Utility;
+import com.shopme.address.AddressService;
+import com.shopme.common.entity.Address;
 import com.shopme.common.entity.CartItem;
 import com.shopme.common.entity.Customer;
+import com.shopme.common.entity.ShippingRate;
 import com.shopme.common.exception.CustomerNotFoundException;
 import com.shopme.customer.CustomerService;
+import com.shopme.shipping.ShippingRateService;
 
 @Controller
 public class ShoppingCartController {
 	
 	@Autowired private ShoppingCartService cartService;
 	@Autowired private CustomerService customerService;
+	@Autowired private AddressService addressService;
+	@Autowired private ShippingRateService shipService; 
 	
 	@GetMapping("/cart")
 	public String viewCart(Model model, HttpServletRequest request) {
@@ -32,6 +38,19 @@ public class ShoppingCartController {
 			estimatedTotal += item.getSubtotal();
 		}
 		
+		Address defaultAddress = addressService.getDefaultAddress(customer);
+		ShippingRate shippingRate = null;
+		boolean usePrimaryAddressAsDefault = false;
+		
+		if (defaultAddress != null) {
+			shippingRate = shipService.getShippingRateForAddress(defaultAddress);
+		} else {
+			usePrimaryAddressAsDefault = true;
+			shippingRate = shipService.getShippingRateForCustomer(customer);
+		}
+		
+		model.addAttribute("usePrimaryAddressAsDefault", usePrimaryAddressAsDefault);
+		model.addAttribute("shippingSupported", shippingRate != null);
 		model.addAttribute("cartItems", cartItems);
 		model.addAttribute("estimatedTotal", estimatedTotal);
 		
