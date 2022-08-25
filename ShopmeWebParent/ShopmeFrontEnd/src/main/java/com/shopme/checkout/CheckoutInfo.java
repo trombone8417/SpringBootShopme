ed35b150d008864a89a66fa77ae685450a8312d3
trombone8417 <trombone8417@yahoo.com>
2022-08-26 07:39:55 +0800

    Code Checkout Page v1

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutInfo.java
@@ -0,0 +1,60 @@
+package com.shopme.checkout;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CheckoutInfo {
+	private float productCost;
+	private float productTotal;
+	private float shippingCostTotal;
+	private float paymentTotal;
+	private int deliverDays;
+	private Date deliverDate;
+	private boolean codSupported;
+	public float getProductCost() {
+		return productCost;
+	}
+	public void setProductCost(float productCost) {
+		this.productCost = productCost;
+	}
+	public float getProductTotal() {
+		return productTotal;
+	}
+	public void setProductTotal(float productTotal) {
+		this.productTotal = productTotal;
+	}
+	public float getShippingCostTotal() {
+		return shippingCostTotal;
+	}
+	public void setShippingCostTotal(float shippingCostTotal) {
+		this.shippingCostTotal = shippingCostTotal;
+	}
+	public float getPaymentTotal() {
+		return paymentTotal;
+	}
+	public void setPaymentTotal(float paymentTotal) {
+		this.paymentTotal = paymentTotal;
+	}
+	public int getDeliverDays() {
+		return deliverDays;
+	}
+	public void setDeliverDays(int deliverDays) {
+		this.deliverDays = deliverDays;
+	}
+	public Date getDeliverDate() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.DATE, deliverDays);
+		return calendar.getTime();
+	}
+	public void setDeliverDate(Date deliverDate) {
+		this.deliverDate = deliverDate;
+	}
+	public boolean isCodSupported() {
+		return codSupported;
+	}
+	public void setCodSupported(boolean codSupported) {
+		this.codSupported = codSupported;
+	}
+	
+	
+}
