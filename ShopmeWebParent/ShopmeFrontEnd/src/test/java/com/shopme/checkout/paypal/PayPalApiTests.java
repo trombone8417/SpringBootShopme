0ac6a6b438dde6f855775617ae48c5053efb5706
trombone8417 <trombone8417@yahoo.com>
2022-09-21 01:35:16 +0800

    Code Validate PayPal Order v1

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/checkout/paypal/PayPalApiTests.java
@@ -0,0 +1,47 @@
+package com.shopme.checkout.paypal;
+
+import java.util.Arrays;
+
+import javax.print.attribute.standard.Media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+public class PayPalApiTests {
+	private static final String BASE_URL = "https://api.sandbox.paypal.com";
+	private static final String GET_ORDER_API = "/v2/checkout/orders/";
+	private static final String CLIENT_ID = "";
+	private static final String CLIENT_SECRET = "";
+	
+	@Test
+	public void testGetOrderDetails() {
+		String orderId = "123abc";
+		String requestURL = BASE_URL + GET_ORDER_API + orderId;
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.add("Accept-Language", "en_US");
+		headers.setBasicAuth(CLIENT_ID, CLIENT_SECRET);
+		
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(headers);
+		RestTemplate restTemplate = new RestTemplate();
+		
+		ResponseEntity<String> response = restTemplate.exchange(requestURL, HttpMethod.GET, request, String.class);
+		
+		System.out.println(response);
+	}
+}
+
+
+
+
+
+
+
+
