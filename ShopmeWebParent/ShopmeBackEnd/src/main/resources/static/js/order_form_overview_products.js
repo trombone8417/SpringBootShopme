2ced1ecf816516e96ca04b9541a5deb52fac2fa4
trombone8417 <trombone8417@yahoo.com>
2022-10-09 16:35:28 +0800

    Code Edit Products in an Order v3

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_overview_products.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_overview_products.js
@@ -12,7 +12,41 @@ $(document).ready(function() {
 	fieldTotal = $("#total");
 	
 	formatOrderAmounts();
+	formatProductAmounts();
+	
+	$("#productList").on("change", ".quantity-input", function(e) {
+		updateSubtotalWhenQuantityChanged($(this));
+	});
 });
+
+function updateSubtotalWhenQuantityChanged(input) {
+	quantityValue = input.val();
+	rowNumber = input.attr("rowNumber");
+	priceField = $("#price" + rowNumber);
+	priceValue = parseFloat(priceField.val().replace(",",""));
+	newSubtotal = parseFloat(quantityValue) * priceValue;
+	
+	subtotalField = $("#subtotal" + rowNumber);
+	subtotalField.val($.number(newSubtotal, 2));
+}
+
+function formatProductAmounts() {
+	$(".cost-input").each(function(e) {
+		formatNumberForField($(this));
+	});
+	
+	$(".price-input").each(function(e) {
+		formatNumberForField($(this));
+	});
+	
+	$(".subtotal-output").each(function(e) {
+		formatNumberForField($(this));
+	});
+	
+	$(".ship-input").each(function(e) {
+		formatNumberForField($(this));
+	});
+}
 
 function formatOrderAmounts() {
 	formatNumberForField(fieldProductCost);
