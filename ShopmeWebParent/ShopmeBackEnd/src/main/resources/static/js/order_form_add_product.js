ccf66c6d4ea3e4603cf60b925ffbf421e04df1ae
trombone8417 <trombone8417@yahoo.com>
2022-10-22 19:19:41 +0800

    Code Add Products to an Order (Part 2) v2

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_add_product.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_add_product.js
@@ -13,8 +13,36 @@ $(document).ready(function() {
 });
 
 function addProduct(productId, productName) {
-	$("#addProductModal").modal("hide");
-	showWarningModal("Product is not added.");
+	getShippingCost(productId);
+}
+
+function getShippingCost(productId) {
+	selectedCountry = $("#country option:selected");
+	countryId = selectedCountry.val();
+	
+	state = $("#state").val();
+	if (state.length == 0) {
+		state = $("#city").val();
+	}
+	
+	requestUrl = contextPath + "get_shipping_cost";
+	params = {productId: productId, countryId: countryId, state: state};
+	
+	$.ajax({
+        type: "POST",
+        url: requestUrl,
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader(csrfHeaderName, csrfValue);
+        },
+        data: params
+        
+    }).done(function(shippingCost) {
+        alert("Shipping cost = " + shippingCost);
+    }).fail(function(err) {
+        showWarningModal(err.responseJSON.message);
+    }).always(function(){
+		$("#addProductModal").modal("hide");
+	});
 }
 
 function isProductAlreadyAdded(productId) {
