aa2bd00d7e325d6870b1f95692d49431be2dbc72
trombone8417 <trombone8417@yahoo.com>
2022-10-23 19:10:09 +0800

    Code Add Products to an Order (Part 2) v3

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRestController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRestController.java
@@ -1,8 +1,13 @@
 package com.shopme.admin.product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shopme.common.entity.product.Product;
+import com.shopme.common.exception.ProductNotFoundException;
 @RestController
 public class ProductRestController {
 	@Autowired private ProductService service;
@@ -11,4 +16,27 @@ public class ProductRestController {
 	public String checkUnique(@Param("id") Integer id, @Param("name") String name) {
 		return service.checkUnique(id, name);
 	}	
+    
+    @GetMapping("/products/get/{id}")
+    public ProductDTO getProductInfo(@PathVariable("id") Integer id) 
+            throws ProductNotFoundException {
+        Product product = service.get(id);
+        return new ProductDTO(product.getName(), product.getMainImagePath(),
+                product.getDiscountPrice(), product.getCost());
+    }
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
