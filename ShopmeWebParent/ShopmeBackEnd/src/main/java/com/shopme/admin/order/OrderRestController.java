8afd244ca4d0bfc1246a2d598927860b1a1de5fa
trombone8417 <trombone8417@yahoo.com>
2022-11-11 20:41:27 +0800

    Code Service and Controller for Updating Order Status v2

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderRestController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderRestController.java
@@ -8,25 +8,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderRestController {
 
-    @Autowired private OrderService service;
-    
+    @Autowired
+    private OrderService service;
+
     @PostMapping("/orders_shipper/update/{id}/{status}")
-    public void updateOrderStatus(@PathVariable("id") Integer orderId,
+    public Response updateOrderStatus(@PathVariable("id") Integer orderId,
             @PathVariable("status") String status) {
         service.updateStatus(orderId, status);
+        return new Response(orderId, status);
     }
-    
+
 }
 
+class Response {
+    private Integer orderId;
+    private String status;
 
+    public Response(Integer orderId, String status) {
+        this.orderId = orderId;
+        this.status = status;
+    }
 
+    public Integer getOrderId() {
+        return orderId;
+    }
 
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
 
+    public String getStatus() {
+        return status;
+    }
 
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-
-
-
-
-
-
+}
