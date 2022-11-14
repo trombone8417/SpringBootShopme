3c30d28e09bd58437b01bb49ddffd84eddc32ba2
trombone8417 <trombone8417@yahoo.com>
2022-11-14 20:19:45 +0800

    Code View Layer for Updating Order Status v2

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/orders_shipper.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/orders_shipper.js
@@ -1,10 +1,37 @@
+var confirmText;
+var confirmModalDialog;
+
+
 $(document).ready(function(){
+	confirmText = $("#confirmText");
+    confirmModalDialog = $("#confirmModal");
 	$(".linkUpdateStatus").on("click", function(e) {
 		e.preventDefault();
 		link = $(this);
-		alert(link.attr("href"));
+		showUpdateConfirmModal(link);
 	});
 });
+
+
+function showUpdateConfirmModal(link){
+	orderId = link.attr("orderId");
+	status = link.attr("status");
+	
+	confirmText.text("Are you sure you want to update status of the order ID #" + orderId
+						+ " to " + status + "?");
+	confirmModalDialog.modal();
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
