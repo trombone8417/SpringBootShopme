9c23ed13fb15a9646d3c3d325c929e1837ee1d6f
trombone8417 <trombone8417@yahoo.com>
2022-11-15 20:28:43 +0800

    Code View Layer for Updating Order Status v3

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/orders_shipper.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/orders_shipper.js
@@ -1,24 +1,52 @@
 var confirmText;
 var confirmModalDialog;
+var yesButton;
 
 
 $(document).ready(function(){
 	confirmText = $("#confirmText");
     confirmModalDialog = $("#confirmModal");
+    yesButton = $("#yesButton");
+    
 	$(".linkUpdateStatus").on("click", function(e) {
 		e.preventDefault();
 		link = $(this);
 		showUpdateConfirmModal(link);
 	});
+	
+	addEventHandlerForYesButton();
 });
 
+function addEventHandlerForYesButton(){
+	yesButton.click(function(e) {
+		e.preventDefault();
+		sendRequestToUpdateOrderStatus($(this));
+	})
+}
+
+function sendRequestToUpdateOrderStatus(button){
+	requestURL = button.attr("href");
+	
+	$.ajax({
+        type: "POST",
+        url: requestURL,
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader(csrfHeaderName, csrfValue);
+        }
+    }).done(function(response) {
+		console.log(response);
+    }).fail(function(err) {
+    });
+}
 
 function showUpdateConfirmModal(link){
 	orderId = link.attr("orderId");
 	status = link.attr("status");
+	yesButton.attr("href", link.attr("href"));
 	
 	confirmText.text("Are you sure you want to update status of the order ID #" + orderId
 						+ " to " + status + "?");
+						
 	confirmModalDialog.modal();
 }
 
